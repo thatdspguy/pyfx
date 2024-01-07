8ac9e78c9a9822aaff8e794ae389f1ca5016149f
keatoncscheible <keatoncscheible@gmail.com>
2024-01-07 05:57:54 -0800

    Remove unused exceptions

--- src/pyfx/exceptions.py
+++ src/pyfx/exceptions.py
@@ -31,36 +31,6 @@ class PedalDoesNotExistError(Exception):
         super().__init__()
 
 
-class InvalidPedalConfigError(Exception):
-    def __init__(self):
-        super().__init__()
-
-
-class UnsavedPedalChangesError(Exception):
-    def __init__(self):
-        super().__init__()
-
-
-class PedalOverwriteError(Exception):
-    def __init__(self):
-        super().__init__()
-
-
-class PedalConfigNotFoundError(Exception):
-    def __init__(self):
-        super().__init__()
-
-
-class NewPedalConfigError(Exception):
-    def __init__(self, message: Optional[str] = None):
-        super().__init__(message)
-
-
-class PedalVariantAlreadyExistsError(Exception):
-    def __init__(self):
-        super().__init__()
-
-
 class PedalVariantDoesNotExistError(Exception):
     def __init__(self):
         super().__init__()
